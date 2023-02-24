6213bb5e7fb28e2ee6a748deaf16684e35a5d6f5
kdmitry0688 <111080338+kdmitry0688@users.noreply.github.com>
2023-02-24 19:04:34 +0300

    Add files via upload

---
+++ Seminar3/Task9/Supplier.java
@@ -0,0 +1,5 @@
+package Seminar3.Task9;
+
+public interface Supplier <T> {
+    T get();
+}
---
+++ Seminar3/Task9/Test.java
@@ -0,0 +1,8 @@
+package Seminar3.Task9;
+
+public class Test {
+    public static void main(String[] args) {
+        
+    }
+
+}
